c38cbed91a69b8920c28e51999bd96a923e19537
sagarjadhav968 <sagarjdhv968@gmail.com>
2019-09-16 13:52:12 +0530

    first commit

---
+++ src/main/java/com/menu/rowtotreecreation/RowtotreecreationApplication.java
@@ -0,0 +1,13 @@
+package com.menu.rowtotreecreation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RowtotreecreationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RowtotreecreationApplication.class, args);
+	}
+
+}
